891b945089194c8c32afe02fcb4743384fe1712d
Wong Keng <kaira428@gmail.com>
2023-03-20 20:27:34 +0800

    Add CountCard component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,46 @@
 import { Container, Paper, Box, Stack, Grid } from "@mui/material";
-import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 import "./App.css";
+import CountCard from "./components/CountCard";
 
 function App() {
   return (
     <>
       <Container>
-        <Grid container spacing={2} xs={12} md={3} sx={{display: "inline-block", backgroundColor: "green"}} direction="column">
-          <Grid item>
-            Test 1
-          </Grid>
-          <Grid item>
-            Test 2
-          </Grid>
+        <Grid
+          container
+          spacing={2}
+          sx={{ display: "inline-block"}}
+          direction="column"
+          item xs={12} md={3}
+          padding={2}
+        >
+          <CountCard />
+          <CountCard />
+          <CountCard />
         </Grid>
-        <Grid container spacing={2} xs={12} md={6} sx={{display: "inline-block", backgroundColor: "lightblue"}} direction="column">
-          <Grid item>
-            Test 3
-          </Grid>
-          <Grid item>
-            Test 4
-          </Grid>
+        <Grid
+          container
+          spacing={2}
+          sx={{ display: "inline-block"}}
+          direction="column"
+          item xs={12} md={6}
+          padding={2}
+        >
+          <CountCard />
+          <CountCard />
         </Grid>
-        <Grid container spacing={2} xs={12} md={3} sx={{display: "inline-block", backgroundColor: "pink"}} direction="column">
-          <Grid item>
-            Test 5
-          </Grid>
-          <Grid item>
-            Test 6
-          </Grid>
-        </Grid>        
+        <Grid
+          container
+          spacing={2}
+          sx={{ display: "inline-block"}}
+          direction="column"
+          item xs={12} md={3}
+          padding={2}
+        >
+          <CountCard />
+          <CountCard />
+          <CountCard />
+        </Grid>
       </Container>
     </>
   );
---
+++ src/components/CountCard.js
@@ -0,0 +1,12 @@
+import { Grid, Paper } from "@mui/material";
+import React from "react";
+
+const CountCard = () => {
+  return (
+    <Grid item>
+      <Paper elevation={3}>Test 1</Paper>
+    </Grid>
+  );
+};
+
+export default CountCard;
