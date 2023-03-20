faeb807432575ef00ea93cc4ef6cbb29bfb39ea2
Wong Keng <kaira428@gmail.com>
2023-03-20 20:10:57 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Paper, Box, Stack, Grid } from "@mui/material";
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container>
+        <Grid container spacing={2} xs={12} md={3} sx={{display: "inline-block", backgroundColor: "green"}} direction="column">
+          <Grid item>
+            Test 1
+          </Grid>
+          <Grid item>
+            Test 2
+          </Grid>
+        </Grid>
+        <Grid container spacing={2} xs={12} md={6} sx={{display: "inline-block", backgroundColor: "lightblue"}} direction="column">
+          <Grid item>
+            Test 3
+          </Grid>
+          <Grid item>
+            Test 4
+          </Grid>
+        </Grid>
+        <Grid container spacing={2} xs={12} md={3} sx={{display: "inline-block", backgroundColor: "pink"}} direction="column">
+          <Grid item>
+            Test 5
+          </Grid>
+          <Grid item>
+            Test 6
+          </Grid>
+        </Grid>        
+      </Container>
+    </>
   );
 }
 
