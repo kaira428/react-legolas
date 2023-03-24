d45b6cf6d21bd7f2ba2579929c596f5e914d247d
Wong Keng <kaira428@gmail.com>
2023-03-24 21:58:25 +0800

    Add sample data, courses.js

---
+++ src/data/courses.js
@@ -0,0 +1,94 @@
+const courses = [
+    {
+        "id": 1,
+        "name": "Java Developer",
+        "cohorts": [
+            {
+                "cohortNum": 9,
+                "startDate": "20-03-2022",
+                "endDate": "20-05-2022",
+                "mentorName": "WK",
+                "coach": "Benny",
+                "country": "Singapore"
+            },
+            {
+                "cohortNum": 11,
+                "startDate": "20-03-2022",
+                "endDate": "20-05-2022",
+                "mentorName": "WK",
+                "coach": "Ben",
+                "country": "Singapore"
+            },
+            {
+                "cohortNum": 13,
+                "startDate": "20-03-2022",
+                "endDate": "20-05-2022",
+                "mentorName": "WK",
+                "coach": "Benny",
+                "country": "Philippines"
+            },
+        ]
+    },
+    {
+        "id": 2,
+        "name": "JavaScript Developer",
+        "cohorts": [
+            {
+                "cohortNum": 10,
+                "startDate": "20-03-2022",
+                "endDate": "20-05-2022",
+                "mentorName": "WK",
+                "coach": "Ben",
+                "country": "Singapore"
+            },
+            {
+                "cohortNum": 12,
+                "startDate": "20-03-2022",
+                "endDate": "20-05-2022",
+                "mentorName": "Thendral",
+                "coach": "Ben",
+                "country": "Philippines"
+            },
+            {
+                "cohortNum": 20,
+                "startDate": "20-03-2022",
+                "endDate": "20-05-2022",
+                "mentorName": "HB",
+                "coach": "Benny",
+                "country": "Singapore"
+            },
+        ]
+    },
+    {
+        "id": 3,
+        "name": "DevOps Developer",
+        "cohorts": [
+            {
+                "cohortNum": 14,
+                "startDate": "20-03-2022",
+                "endDate": "20-05-2022",
+                "mentorName": "Thendral",
+                "coach": "Benny",
+                "country": "Philippines"
+            },
+            {
+                "cohortNum": 11,
+                "startDate": "20-03-2022",
+                "endDate": "20-05-2022",
+                "mentorName": "HB",
+                "coach": "Ben",
+                "country": "Singapore"
+            },
+            {
+                "cohortNum": 15,
+                "startDate": "20-03-2022",
+                "endDate": "20-05-2022",
+                "mentorName": "Thendral",
+                "coach": "Ben",
+                "country": "Philippines"
+            },
+        ]
+    },
+]
+
+export default courses;
